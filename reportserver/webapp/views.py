37f2230a622c78e6655b8ac91562f81fb1996aba
novikov.sergey <novikov.sergey@otr.ru>
2022-06-03 17:31:29 +0300

    реализовано формирование формы с параметрами отчета

--- reportserver/webapp/views.py
+++ reportserver/webapp/views.py
@@ -1,42 +1,69 @@
 import os.path
 import logging
+import datetime
 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect, FileResponse
 from django.views.generic.edit import CreateView
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse_lazy
-from .forms import RunReport, RunReportParameters
+from .forms import RunReport
 
 from django.contrib.auth.models import User
 from django.contrib.auth import logout
 from django import forms
 from django.conf import settings
 
-from .models import Tasks, States
+from .models import Tasks, States, Report, ReportParameters
 
 logger = logging.getLogger(__name__)
 
-
 @login_required(login_url='accounts/login/')
-def run_report(request):
+def sel_report(request):
     if request.method == 'POST':
         form = RunReport(request.POST or None)
         if form.is_valid():
             task = form.save(commit=False)
-            user = User.objects.get(username=request.user.username)
-            task.userLogin = user
-            task.report = form.cleaned_data['report']
-            task.reportParameters = form.cleaned_data['reportParameters']
-            task.save()
-
-            return HttpResponseRedirect('/')
+            report_pk = task.report.pk
+            return HttpResponseRedirect(str(report_pk))
     else:
         form = RunReport()
         form.fields['userLogin'].widget = forms.HiddenInput()
-        form_parameters = RunReportParameters()
+    return render(request, 'webapp/sel_report.html', {'form': form})
 
-    return render(request, 'webapp/setparamreport.html', {'form': form})
+@login_required(login_url='accounts/login/')
+def run_report(request, pk):
+    if request.method == 'POST':
+        task = Tasks()
+        task.report = Report.objects.get(pk=pk)
+        task.state = States.objects.get(systemName='NEW')
+        task.startReportDateTime = datetime.datetime.now()
+
+        list_parameters = []
+        for parametr in request.POST:
+            if parametr != 'csrfmiddlewaretoken':
+                list_parameters.append(parametr+"="+request.POST[parametr])
+        task.reportParameters = ";".join(list_parameters)
+        task.userLogin = User.objects.get_by_natural_key(request.user.username)
+        task.save()
+        return HttpResponseRedirect('/')
+    else:
+        report = Report.objects.get(pk=pk)
+        report_name = report.name
+        report_parameters = ReportParameters.objects.filter(report=report).order_by('number_in_order')
+        parameters = {}
+        for parameter in report_parameters:
+            parameters[parameter.parameter_systemname] = parameter.parameter_name
+
+
+        content = {'parameters_report': parameters, 'report_name': report_name}
+
+
+    return render(request, 'webapp/setparamreport.html', content)
+
+
+
+
 
 
 @login_required(login_url='accounts/login/')
